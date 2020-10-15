b49c4c86a92bebce1d20a827a95afda71d60c0ca
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-15 14:51:22 +0900

    write: sigmoidForward() (activation\sigmoid.c)

--- activations/sigmoid.c
+++ activations/sigmoid.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<math.h>
 
-void sigmoidForward()
+void sigmoidForward(float *x, float *y, int len)
 {
-    
+    for (int i=0; i<len; i++)
+        *(y + i) = 1.0 / (1.0 + expf(-*(x + i)));
 }
 
-void sigmoidBackward()
+void sigmoidBackward(float *deltaLossX, float *deltaLossY, float *y, int xLen)
 {
-
 }

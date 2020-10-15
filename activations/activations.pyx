a1013bbe120f81b58409e039c6a4e8ddd97f93ea
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-15 15:23:09 +0900

    rename data_len --> dataLen (activations\activations.pyx)

--- activations/activations.pyx
+++ activations/activations.pyx
@@ -3,8 +3,8 @@ import numpy as np
 cimport numpy as np
 
 cdef extern from "sigmoid.h":
-    void sigmoidForward(float *x, float *y, int data_len);
-    void sigmoidBackward(float *deltaLoss, float *Loss, float *y, int data_len);
+    void sigmoidForward(float *x, float *y, int dataLen);
+    void sigmoidBackward(float *deltaLoss, float *Loss, float *y, int dataLen);
 
 DTYPE = np.float32
 ctypedef np.float32_t DTYPE_t

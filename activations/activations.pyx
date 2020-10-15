e44716d1f1bb2199d74e891bd994ea83fdbc092d
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-15 15:18:49 +0900

    write: link to functions of "activations\sigmoid.h" (activations\activations.pyx)

--- activations/activations.pyx
+++ activations/activations.pyx
@@ -2,6 +2,10 @@ import cython
 import numpy as np
 cimport numpy as np
 
+cdef extern from "sigmoid.h":
+    void sigmoidForward(float *x, float *y, int data_len);
+    void sigmoidBackward(float *deltaLoss, float *Loss, float *y, int data_len);
+
 DTYPE = np.float32
 ctypedef np.float32_t DTYPE_t
 

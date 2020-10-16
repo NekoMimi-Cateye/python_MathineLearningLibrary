e1b98fb5d3ced45ae0e6b2e0445a71ad695fb485
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-16 12:11:54 +0900

    debug: add "self" argument (activations\activations.pyx)

--- activations/activations.pyx
+++ activations/activations.pyx
@@ -12,12 +12,12 @@ ctypedef np.float32_t DTYPE_t
 cdef class Sigmoid:
     cdef np.ndarray y;
 
-    def forward(np.ndarray x):
+    def forward(self, np.ndarray x):
         dataSize = x.shape
         dataLen = x.size
         self.y = np.empty(dataSize, DTYPE)
         sigmoidForward(<float *> x, <float *> self.y, dataLen)
         return(self.y)
 
-    def backward():
+    def backward(self):
         pass

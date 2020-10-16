b9ff3bdc9f81daebb374fba49a71d854b0646cdf
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-17 03:06:26 +0900

    complete Version1.0 (not debug)

--- Model.py
+++ Model.py
@@ -1,9 +1,54 @@
+import numpy
+
 class Model:
     def __init__(self):
-        pass
+        self.model = []
+        self.lossFunction = []
+        self.wlayerPointer = []
+        self.optimizer = []
+        self.logLoss = []
+        self.logEpoch = []
 
-    def predict(self):
-        pass
+    def addLayer(self, layer, haveWeight):
+        self.model.append(layer)
+        if haveWeight:
+            self.wlayerPointer.append(len(self.model)-1)
 
-    def train(self):
-        pass
+    def addLossFunction(self, lossFunction):
+        self.lossFunction.append(lossFunction)
+
+    def addOptimizer(self, optimizer):
+        self.optimizer.append(optimizer)
+
+    def predict(self, x):
+        y = self.model[0].forward(x)
+        for m in self.model[1:]:
+            y = m.forward(y)
+        return y
+
+    def backward(self):
+        deltaLoss = self.lossFunction[0].backward()
+        for m in self.model[-1:]:
+            deltaLoss = m.backward(deltaLoss)
+        
+
+
+    def train(self, dataX, dataT, epochs):
+        self.logLoss = [0.0 for i in range(epochs)]
+        self.logEpoch = [i+1 for i in range(epochs)]
+        for e in range(epochs):
+            ##--step1--##
+            dataY = self.predict(dataX)
+
+            ##--step2--## #in production
+            loss = self.lossFunction[0].forward(dataY, dataT)
+
+            ##--step3--##
+            self.backward()
+
+            ##--step4--##
+            for i in self.wlayerPointer:
+                self.optimizer[0].update(self.model[i].weight, self.model[i].deltaLossW)
+
+            ##--step5--##
+            self.logLoss[e] = loss
--- optimizer/sgd.py
+++ optimizer/sgd.py
@@ -4,5 +4,5 @@ class SGD:
     def __init__(self, learningRate):
         self.learningRate = learningRate
 
-    def sgd(self, weight, lossWeight):
+    def update(self, weight, lossWeight):
         weight -= weight * self.learningRate

30d1d066b7652c0f4270829d6315533d4f1a8215
NekoMimi-Cateye <umakko.lumiere@icloud.com>
2020-10-16 12:06:22 +0900

    write: write code of Sigmoid -> forward() (activations\activations.pyx)

--- activations/activations.pyx
+++ activations/activations.pyx
@@ -12,6 +12,11 @@ ctypedef np.float32_t DTYPE_t
 cdef class Sigmoid:
     cdef np.ndarray y;
 
-    def forward():
+    def forward(np.ndarray x):
+        dataSize = x.shape
+        dataLen = x.size
+        self.y = np.empty(dataSize, DTYPE)
+        sigmoidForward(<float *> x, <float *> self.y, dataLen)
+        return(self.y)
 
     def backward():
